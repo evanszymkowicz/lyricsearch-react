ce5383290b0e9c9c4d45c0f07ac3494c59221bab
evanszymkowicz <evan.szymkowicz@evanwolf.co>
2019-04-30 14:56:24 -0400

    some boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+  );
+}
+
+export default App;
